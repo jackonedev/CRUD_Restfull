0ad104dc29b80fe5196b96a0ec2d72c999ecf417
AgustinSt1990 <jackone.action.software@gmail.com>
2022-12-07 13:00:43 -0300

    ended models.py > Profile with validations

--- django_project/rest_application/models.py
+++ django_project/rest_application/models.py
@@ -13,12 +13,18 @@ def valid_name(value):
         raise ValidationError(f'Name must be a string, not {value}')
     return value
 
+def valid_age(value):
+    value = str(value)
+    if not value.isdigit():
+        raise ValidationError(f'Age must be a number, not {value}')
+    return value
+
 class Profile(models.Model):
     personal_id = models.CharField(primary_key=True, max_length=50, unique=True, validators=[valid_personal_id])
     name = models.CharField(max_length=50, validators=[valid_name])
     last_name = models.CharField(max_length=50, validators=[valid_name])
-    age = models.PositiveIntegerField()
-    profile_image = models.ImageField(upload_to='profile/profile_image', blank=True)
+    age = models.PositiveIntegerField(validators=[valid_age])
+    profile_image = models.ImageField(upload_to='profile/profile_image', blank=True, null=True)
 
     def __str__(self):
         s = f'{self.name} {self.last_name}, ({self.personal_id})'
