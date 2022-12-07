19fc59e857054adeb1cb9381b8d1246b9367b825
AgustinSt1990 <jackone.action.software@gmail.com>
2022-12-07 11:56:02 -0300

    updated api-rest views

--- django_project/rest_application/api/views.py
+++ django_project/rest_application/api/views.py
@@ -2,19 +2,65 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
+from ..application_package.get_query_params import get_query_params
+
 from ..models import Profile
 from .serializers import ProfileSerializer
+
+
+
 
 
 @api_view(['GET', 'POST'])
 def get_post_profile(request):
     if request.method == 'GET':
-        profiles = Profile.objects.all()
+        try:
+            print ('INGRESANDO A GET AND TRY')
+            profiles = get_query_params(request, Profile)
+            
+            if profiles == 'not found':
+                return Response(status=status.HTTP_404_NOT_FOUND)
+
+            if profiles == 'bad request':
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        except:
+            print ("INGRESANDO A GET AND EXCEPT")
+            profiles = Profile.objects.all()
         serializer = ProfileSerializer(profiles, many=True)
         return Response(serializer.data)
+
+
     elif request.method == 'POST':
         serializer = ProfileSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def get_put_delete_profile(request, pk):
+    try:
+        profile = Profile.objects.get(pk=pk)
+    except Profile.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = ProfileSerializer(profile)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        profile.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
