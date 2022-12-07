853d8986db8f3bb2dabbcffb5d68498387260387
AgustinSt1990 <jackone.action.software@gmail.com>
2022-12-07 08:08:36 -0300

    CREATED AND REGISTERED FIRST MODEL 'Profile'

--- django_project/rest_application/models.py
+++ django_project/rest_application/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+
+def valid_personal_id(value):
+    value = value.replace('-', '').replace('.', '')
+    if not value.isdigit():
+        raise ValidationError(f'Personal ID must be a number, not {value}')
+    return value
+
+def valid_name(value):
+    if not value.isalpha():
+        raise ValidationError(f'Name must be a string, not {value}')
+    return value
+
+class Profile(models.Model):
+    personal_id = models.CharField(primary_key=True, max_length=50, unique=True, validators=[valid_personal_id])
+    name = models.CharField(max_length=50, validators=[valid_name])
+    last_name = models.CharField(max_length=50, validators=[valid_name])
+    age = models.PositiveIntegerField()
+    profile_image = models.ImageField(upload_to='profile/profile_image', blank=True)
+
+    def __str__(self):
+        s = f'{self.name} {self.last_name}, ({self.personal_id})'
+        return s
