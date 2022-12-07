3c9c0acad13c04912080544e2ee6f9ebbc99dc6e
AgustinSt1990 <jackone.action.software@gmail.com>
2022-12-07 13:01:35 -0300

    added url with endpoint 2 'api/v1/profiles/<id>'

--- django_project/rest_application/api/urls.py
+++ django_project/rest_application/api/urls.py
@@ -5,5 +5,6 @@ app_name = 'api-rest'
 from . import views
 
 urlpatterns = [
-    path('profiles/', views.get_post_profile, name='get_post_profile'),
+    path('profiles/', views.get_post_profile, name='get_post'),
+    path('profiles/<int:pk>/', views.get_put_delete_profile, name='get_put_delete'),
 ]
