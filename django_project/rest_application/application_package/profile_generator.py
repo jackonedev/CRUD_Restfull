a051f524ab06690d840c0a885ab44c49665cc035
AgustinSt1990 <jackone.action.software@gmail.com>
2022-12-09 08:46:05 -0300

    created 'profile_generator' in application_package

---
+++ django_project/rest_application/application_package/profile_generator.py
@@ -0,0 +1,76 @@
+import os, random, pprint, json
+from itertools import count
+
+
+def get_random_person():
+    "Funcion que retorna un diccionario con los datos de una persona genérica"
+    age = get_random_age()
+    id = get_id(age)
+    name, last_name = get_random_name()
+    return json.dumps({'personal_id': id, 'name': name, 'last_name': last_name, 'age': age})
+
+
+def get_random_age():
+    return random.randint(18, 100)
+
+def recta_y(age):
+    if age >= 18 and age <= 36:
+        x1, x2 = 18, 36
+        y1, y2 = 3e6, 1e6
+
+    if age > 36 and age <= 65:
+        x1, x2 = 37, 65
+        y1, y2 = 760000, 250000
+
+    if age > 65 and age <= 100:
+        x1, x2 = 66, 100
+        y1, y2 = 162000, 55000
+    return int((y2 - y1) / (x2 - x1) * (age - x1) + y1)
+
+def get_id(age):
+    global counter
+    root_id = recta_y(age)
+    plus = next(counter)
+    return root_id + plus
+    
+
+def get_files(path):
+    files = []
+    for file in os.listdir(path):
+        if os.path.isfile(os.path.join(path, file)):
+            files.append(file)
+    return files
+
+
+def get_directories(path):
+    directories = []
+    for directory in os.listdir(path):
+        if os.path.isdir(os.path.join(path, directory)):
+            directories.append(directory)
+    return directories
+
+
+def get_random_line(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+        return random.choice(lines).rstrip()
+
+def get_random_name():
+    name = get_random_line('application/package/contenido_generado/nombres.txt')
+    last_name = get_random_line('application/package/contenido_generado/apellidos.txt')
+    return name, last_name
+
+
+def main():
+    global counter
+    counter = count()
+    for i in range(10):
+        print (">>>> Persona Random:")
+        print(get_random_person())
+        print()
+
+
+
+if __name__ == '__main__':
+    
+    main()
