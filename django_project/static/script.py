a517418678a101f31f441aa0c2a45dc4eb217ce4
AgustinSt1990 <jackone.action.software@gmail.com>
2022-12-09 14:22:28 -0300

    completado: se añade al div con id 'form' se le añade la class 'action'

--- django_project/static/script.py
+++ django_project/static/script.py
@@ -21,98 +21,167 @@ async def make_request(url, method, body=None, headers=None):
 )
     return await response.json()
     
-def create_field():
+def create_field(id=None):
     field = document.createElement('input')
     field.type = 'text'
     field.setAttribute('class', 'form-control-sm mb-2 row')
+
+    if id:
+        field.id = id
     return field
 
 
+def create_button(textContent, id, class_name):
+    button = document.createElement('button')
+    button.type = 'button'
+    button.textContent = textContent
+    button.id = id
+    button.setAttribute('class', class_name)
+    return button
+
 def read_download():
     global fragment, form_loc, templateForm
+    form_loc.textContent = ''
+    templateForm.querySelector('#form').textContent = ''
 
-    field_personal_id = create_field()
-    field_name = create_field()
-    field_last_name = create_field()
-    field_age = create_field()
+    field_personal_id = create_field(id='id')
+    field_name = create_field(id='name')
+    field_last_name = create_field(id='last_name')
+    field_age = create_field(id='age')
     
     field_personal_id.placeholder = 'id (* to match the rest)'
     field_name.placeholder = "name"
     field_last_name.placeholder = "last name"
     field_age.placeholder = "age"
 
+    search = create_button('Search', 'search', 'btn btn-primary btn-sm mx-2')
+    export = create_button('Export', 'export', 'btn btn-primary btn-sm mx-2')
     
-    # field_personal_id.className += 'col-xs-4'
-    # field_personal_id.className += 'border border-primary'
-
     templateForm.querySelector('.card-title').textContent = 'Search and Export'
     templateForm.querySelector('#form').appendChild(field_personal_id)
     templateForm.querySelector('#form').appendChild(field_name)
     templateForm.querySelector('#form').appendChild(field_last_name)
     templateForm.querySelector('#form').appendChild(field_age)
+    templateForm.querySelector('#form').appendChild(search)
+    templateForm.querySelector('#form').appendChild(export)
     console.log(templateForm.querySelector('#form'))
 
     clone = templateForm.cloneNode(True)
     fragment.appendChild(clone)
 
     form_loc.appendChild(fragment)
-
-
+    
 def create():
     global fragment, form_loc, templateForm
+    form_loc.textContent = ''
+    templateForm.querySelector('#form').textContent = ''
+
     field_personal_id = create_field()
     field_personal_id.placeholder = 'input exact id'
+
+    create = create_button('Create', 'create', 'btn btn-primary btn-sm mx-2')
 
     templateForm.querySelector('.card-title').textContent = 'Create new profile'
     templateForm.querySelector('#form').appendChild(field_personal_id)
+    templateForm.querySelector('#form').appendChild(create)
     console.log(templateForm.querySelector('#form'))
     clone = templateForm.cloneNode(True)
     fragment.appendChild(clone)
     form_loc.appendChild(fragment)
-
 
 def update_delete():
     global fragment, form_loc, templateForm
+    form_loc.textContent = ''
+    templateForm.querySelector('#form').textContent = ''
+
     field_personal_id = create_field()
     field_personal_id.placeholder = 'input exact id'
 
+    update = create_button('Update', 'update', 'btn btn-primary btn-sm mx-2')
+    delete = create_button('Delete', 'delete', 'btn btn-danger btn-sm mx-2')
+
     templateForm.querySelector('.card-title').textContent = 'Update or Delete profile'
     templateForm.querySelector('#form').appendChild(field_personal_id)
+    templateForm.querySelector('#form').appendChild(update)
+    templateForm.querySelector('#form').appendChild(delete)
     console.log(templateForm.querySelector('#form'))
     clone = templateForm.cloneNode(True)
     fragment.appendChild(clone)
     form_loc.appendChild(fragment)
+
 
 def proxy_container(e):
     global form_loc, templateForm
     console.log(e.target)
 
-    form_loc.textContent = ''
-    templateForm.querySelector('#form').textContent = ''
-
     if e.target.id == 'read-download':
         console.log(e.target.id)
+        templateForm.querySelector('#form').className = ''
         read_download()
+        templateForm.querySelector('#form').classList.add('action-1')
     elif e.target.id == 'create':
         console.log(e.target.id)
+        templateForm.querySelector('#form').className = ''
         create()
+        templateForm.querySelector('#form').classList.add('action-2')
     elif e.target.id == 'update-delete':
         console.log(e.target.id)
+        templateForm.querySelector('#form').className = ''
         update_delete()
+        templateForm.querySelector('#form').classList.add('action-3')
+
+
+
+async def search_data(e):
+    pass
+
+async def export_data(e):
+    pass
+
+async def create_data(e):
+    pass
+
+async def update_data(e):
+    pass
+
+async def delete_data(e):
+    pass
+
+
+
+def proxy_form_location(e):
+    global templateForm
+    console.log(e.target)
+
+    search = templateForm.querySelector('#search')
+    export = templateForm.querySelector('#export')
+    create = templateForm.querySelector('#create')
+    update = templateForm.querySelector('#update')
+    delete = templateForm.querySelector('#delete')
+
+    console.log(search)
+
+    # search.addEventListener('click', create_proxy(search_data))
+    # export.addEventListener('click', create_proxy(export_data))
+    # create.addEventListener('click', create_proxy(create_data))
+    # update.addEventListener('click', create_proxy(update_data))
+    # delete.addEventListener('click', create_proxy(delete_data))
+
 
 
 
 def main():
     global templateForm, form_loc, templateButtons, container
     global fragment
+    fragment = document.createDocumentFragment()
 
     container = document.querySelector('.container')
     form_loc = document.querySelector('#form-loc')
     templateForm = document.getElementById('template-form').content
-    fragment = document.createDocumentFragment()
 
 
     container.addEventListener('click', create_proxy(proxy_container))
+    form_loc.addEventListener('click', create_proxy(proxy_form_location))
 
 
 
