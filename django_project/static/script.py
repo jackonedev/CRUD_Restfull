ec2cf62c9696e907b16656d10f5f037902392871
AgustinSt1990 <jackone.action.software@gmail.com>
2022-12-09 11:37:09 -0300

    ERROR: Cuando hago click sucesivamente se van añadiendo mas formularios con mas lineas

--- django_project/static/script.py
+++ django_project/static/script.py
@@ -19,6 +19,89 @@ async def make_request(url, method, body=None, headers=None):
         body=body,
         headers=default_headers
 )
-
     return await response.json()
     
+def create_field():
+    field = document.createElement('input')
+    field.type = 'text'
+    field.setAttribute('class', 'form-control')
+    return field
+
+
+def read_download():
+    global fragment, form_loc, templateForm
+
+    field_personal_id = create_field()
+    field_name = create_field()
+    field_last_name = create_field()
+    field_age = create_field()
+    
+    
+    templateForm.querySelector('.card-title').textContent = 'Search and Export'
+    templateForm.querySelector('#form').appendChild(field_personal_id)
+    templateForm.querySelector('#form').appendChild(field_name)
+    templateForm.querySelector('#form').appendChild(field_last_name)
+    templateForm.querySelector('#form').appendChild(field_age)
+    console.log(templateForm.querySelector('#form'))
+
+    clone = templateForm.cloneNode(True)
+    fragment.appendChild(clone)
+
+    form_loc.appendChild(fragment)
+
+def create():
+    global fragment, form_loc, templateForm
+    field_personal_id = create_field()
+    field_personal_id.textContent = 'input exact id'
+
+    templateForm.querySelector('.card-title').textContent = 'Create new profile'
+    templateForm.querySelector('#form').appendChild(field_personal_id)
+    console.log(templateForm.querySelector('#form'))
+    clone = templateForm.cloneNode(True)
+    fragment.appendChild(clone)
+    form_loc.appendChild(fragment)
+
+
+def update_delete():
+    global fragment, form_loc, templateForm
+    field_personal_id = create_field()
+    field_personal_id.textContent = 'input exact id'
+
+    templateForm.querySelector('.card-title').textContent = 'Update or Delete profile'
+    templateForm.querySelector('#form').appendChild(field_personal_id)
+    console.log(templateForm.querySelector('#form'))
+    clone = templateForm.cloneNode(True)
+    fragment.appendChild(clone)
+    form_loc.appendChild(fragment)
+
+def proxy_container(e):
+    console.log(e.target)
+
+    if e.target.id == 'read-download':
+        console.log(e.target.id)
+        read_download()
+    elif e.target.id == 'create':
+        console.log(e.target.id)
+        create()
+    elif e.target.id == 'update-delete':
+        console.log(e.target.id)
+        update_delete()
+
+
+
+def main():
+    global templateForm, form_loc, templateButtons
+    global fragment
+
+    container = document.querySelector('.container')
+    form_loc = document.querySelector('#form-loc')
+    templateForm = document.getElementById('template-form').content
+    fragment = document.createDocumentFragment()
+
+
+    container.addEventListener('click', create_proxy(proxy_container))
+
+
+
+
+main()
