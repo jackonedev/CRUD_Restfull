94cb5da6fed2985cbeda03e7553f6c58e62d6d71
AgustinSt1990 <jackone.action.software@gmail.com>
2022-12-09 10:03:45 -0300

    updated 'profile_generator' module

--- django_project/rest_application/application_package/profile_generator.py
+++ django_project/rest_application/application_package/profile_generator.py
@@ -2,21 +2,21 @@ import os, random, pprint, json
 from itertools import count
 
 
-def get_random_person():
+def get_profile(age_input=None):
     "Funcion que retorna un diccionario con los datos de una persona genérica"
-    age = get_random_age()
+    age = age_input or get_random_age()
     id = get_id(age)
     name, last_name = get_random_name()
-    return json.dumps({'personal_id': id, 'name': name, 'last_name': last_name, 'age': age})
+    return {'personal_id': id, 'name': name, 'last_name': last_name, 'age': age}
 
 
 def get_random_age():
     return random.randint(18, 100)
 
-def recta_y(age):
+def recta_id_coef(age):
     if age >= 18 and age <= 36:
         x1, x2 = 18, 36
-        y1, y2 = 3e6, 1e6
+        y1, y2 = 3e6, 900000
 
     if age > 36 and age <= 65:
         x1, x2 = 37, 65
@@ -29,7 +29,7 @@ def recta_y(age):
 
 def get_id(age):
     global counter
-    root_id = recta_y(age) * age
+    root_id = recta_id_coef(age) * age
     plus = next(counter)
     return root_id + plus
     
@@ -50,26 +50,31 @@ def get_directories(path):
     return directories
 
 
-def get_random_line(file_path):
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
-        return random.choice(lines).rstrip()
-
 def get_random_name():
-    name = get_random_line('django_project/rest_application/application_package/contenido_generado/nombres.txt')
-    last_name = get_random_line('django_project/rest_application/application_package/contenido_generado/apellidos.txt')
+    name = random.choice(["Juan", "Pedro", "Maria", "Alejandra", "Jorge", "Mario", "Sofia", "Carola", "Agustin", "Victoria"])
+    last_name = random.choice(["Gris", "Azul", "Verde", "Amarillo", "Rojo", "Naranja", "Violeta", "Bordo"])
     return name, last_name
 
 
 def main():
     global counter
     counter = count()
-    for i in range(10):
-        print (">>>> Persona Random:")
-        print(get_random_person())
-        print()
-
-
-if __name__ == '__main__':
     
-    main()
+    def test_1():
+        for i in range(10):
+            print (">>>> Generated Profile:")
+            print(get_profile())
+            print()
+
+    def test_2():
+        print (get_profile(age_input=32))
+        print (get_profile(age_input=36))
+        print (get_profile(age_input=37))
+        print (get_profile(age_input=65))
+        print (get_profile(age_input=66))
+
+    # test_1()
+    # test_2()
+
+    
+main()
