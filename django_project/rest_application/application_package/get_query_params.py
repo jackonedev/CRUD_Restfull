266f716a5ddc6779862033e2b81a116fba1342ed
AgustinSt1990 <jackone.action.software@gmail.com>
2022-12-10 10:54:38 -0300

    app_package: agrego 'id' como query param en 'get_query_params', que se ejecuta dentro de la view 'get_post_profile'

--- django_project/rest_application/application_package/get_query_params.py
+++ django_project/rest_application/application_package/get_query_params.py
@@ -1,12 +1,30 @@
 def get_query_params(request, model):
+    id = request.query_params.get('id', None)
     name = request.query_params.get('name', None)
     last_name = request.query_params.get('last_name', None)
     age = request.query_params.get('age', None)
 
-    if not (name or last_name or age):
+    if not (name or last_name or age or id):
         return 1/0
 
+    # first search
+    if id:
+        if not id.startswith('*') and not id.endswith('*'):
+            profiles = model.objects.filter(id=id)
+        elif id.startswith('*') and id.endswith('*'):
+            id = id.replace('*', '')
+            profiles = model.objects.filter(id__icontains=id)
+        elif id.startswith('*'):
+            id = id.replace('*', '')
+            profiles = model.objects.filter(id__iendswith=id)
+        elif id.endswith('*'):
+            id = id.replace('*', '')
+            profiles = model.objects.filter(id__istartswith=id)
+        if len(profiles) == 0:
+            profiles = 'not found'
+        return profiles
 
+    # second search
     try:
         name = name.title()
     except:
