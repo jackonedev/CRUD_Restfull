0b2526165071be82ae429602c2dea6262b44eb14
AgustinSt1990 <jackone.action.software@gmail.com>
2022-12-10 10:43:40 -0300

    api.views.py: agrego path 'api/v1/contents/' para la descarga de archivos

--- django_project/rest_application/api/urls.py
+++ django_project/rest_application/api/urls.py
@@ -7,4 +7,5 @@ app_name = 'api-rest'
 urlpatterns = [
     path('profiles/', views.get_post_profile, name='get_post'),
     path('profiles/<pk>/', views.get_put_delete_profile, name='get_put_delete'),
+    path('contents/', views.download_csv, name='get_contents'),
 ]
