d140099a3c4012d222fa3b160ba02e327e64588b
AgustinSt1990 <jackone.action.software@gmail.com>
2022-12-13 19:25:24 -0300

    jupyter_notebooks updated

--- django_project/jupyter_notebooks/django_for_jupyter.py
+++ django_project/jupyter_notebooks/django_for_jupyter.py
@@ -1,4 +1,5 @@
 import os, sys
+
 PWD = os.path.dirname(os.getcwd())
 
 PROJ_MISSING_MSG = """Set an enviroment variable:\n
@@ -7,17 +8,19 @@ or call:\n
 `init_django(your_project_name)`
 """
 
+
 def init_django(project_name=None):
     os.chdir(PWD)
-    project_name = project_name or os.environ.get('DJANGO_PROJECT') or None
+    project_name = project_name or os.environ.get("DJANGO_PROJECT") or None
     if project_name == None:
         raise Exception(PROJ_MISSING_MSG)
     sys.path.insert(0, PWD)
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'{project_name}.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"{project_name}.settings")
     os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
     import django
+
     django.setup()
 
 
 def hello_world():
-    print('Hello World!')
+    print("Hello World!")
